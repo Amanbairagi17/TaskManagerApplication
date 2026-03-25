0fba5063aeec9e976edc7fe4b2f95c07cbe9349d
Amanbairagi17 <amanbairagi741@gmail.com>
2026-03-25 16:48:00 +0530

    [Aman] : Implementation swagger-ui for accessing end-point and testing all APIs for browser

--- src/main/java/com/genius/TaskManager/controller/TaskController.java
+++ src/main/java/com/genius/TaskManager/controller/TaskController.java
@@ -4,6 +4,8 @@ import com.genius.TaskManager.dto.TaskRequestDto;
 import com.genius.TaskManager.dto.TaskResponseDto;
 import com.genius.TaskManager.dto.TaskUpdateDto;
 import com.genius.TaskManager.service.TaskService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,24 +16,28 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "task")
+@Tag(name = "Task Controller", description = "Task management APIs")
 @RequiredArgsConstructor
 public class TaskController {
 
     private final TaskService taskService;
 
     @PostMapping
+    @Operation(summary = "Create new task")
     public ResponseEntity<TaskResponseDto> createTask(@Valid @RequestBody TaskRequestDto taskRequestDto){
         TaskResponseDto response = taskService.createNewTask(taskRequestDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @GetMapping("{id}")
+    @Operation(summary = "Find task by Id")
     public ResponseEntity<TaskResponseDto> getTaskById(@PathVariable long id){
         TaskResponseDto response = taskService.findTaskById(id);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @DeleteMapping("{id}")
+    @Operation(summary = "Delete task by id")
     public ResponseEntity<String> deleteTask(@PathVariable long id){
         String response = taskService.deleteTask(id);
         System.out.println(response);
@@ -41,12 +47,14 @@ public class TaskController {
     }
 
     @PutMapping("{id}")
+    @Operation(summary = "Update task by id")
     public ResponseEntity<TaskResponseDto> updateById(@PathVariable long id, @RequestBody TaskUpdateDto updateDto){
         TaskResponseDto response = taskService.updateTask(id, updateDto);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @GetMapping("/get-all-task")
+    @Operation(summary = "Get All tasks with list")
     public ResponseEntity<List<TaskResponseDto>> getAllTask(){
         List<TaskResponseDto> responseList = taskService.findAllTask();
         return ResponseEntity.status(HttpStatus.OK).body(responseList);
